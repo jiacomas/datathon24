f50e8ae67ba41bae660d08580bd086f8edcfaa67
jiacomas <jcomas.fr@gmail.com>
2024-11-16 13:02:21 +0100

    update clean-data Co-authored-by: Luke Pardo Barahona <lukepardo@users.noreply.github.com>

--- clean_data.py
+++ clean_data.py
@@ -7,6 +7,10 @@ product_data = pd.read_csv('archive/product_data.csv')
 pivoted_attributes = atribute_data.pivot_table(index='cod_modelo_color', columns='attribute_name', values='des_value', aggfunc='first').reset_index()
 product_data2 = pd.merge(product_data, pivoted_attributes, on='cod_modelo_color', how='left')
 
+# Write headers to the new files
+product_data2.head(0).to_csv('bodegon.csv', mode='w', header=True, index=False)
+product_data2.head(0).to_csv('no_bodegon.csv', mode='w', header=True, index=False)
+
 #TODO consider the cod_value
 
 for i in range(len(product_data2)):
@@ -14,3 +18,4 @@ for i in range(len(product_data2)):
         product_data2.iloc[i].to_frame().T.to_csv('bodegon.csv', mode='a', header=False, index=False)
     else:
         product_data2.iloc[i].to_frame().T.to_csv('no_bodegon.csv', mode='a', header=False, index=False)
+
