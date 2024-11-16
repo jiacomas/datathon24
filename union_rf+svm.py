f64203f5526734c71599bf7340b6e882d4905e21
jiacomas <jcomas.fr@gmail.com>
2024-11-16 19:50:21 +0100

    union.py

---
+++ union_rf+svm.py
@@ -0,0 +1,10 @@
+svm_prediction = pd.read_csv('svm_prediction.csv')
+rf_prediction = pd.read_csv('rf_prediction.csv')
+
+# Merge the two predictions
+for index, row in svm_prediction.iterrows():
+    if row['des_value'] == 'INVALID':
+        row['des_value'] = rf_prediction.loc[index, 'des_value']
+    
+svm_prediction.to_csv('rf_u_svm.csv', index=False)
+        
